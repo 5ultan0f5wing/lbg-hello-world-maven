affa361a1f49e5c0cb3fce4cc8ade61a568b667f
Paul Santaana <santaana@sky.com>
2023-02-28 14:58:11 +0000

    Extra Hello message added for everyone

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("PAUL S"));
+        System.out.println(sayHelloToSomeone("Everyone"));
         System.out.println(sayGoodbye());
     }
 
